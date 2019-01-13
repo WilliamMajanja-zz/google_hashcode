f26ddcf19e200c7a7a851e4a35f1d305db815353
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-01-13 13:15:36 +0300

    base: add solution template

--- 2018/base/common.h
+++ 2018/base/common.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #define X first
 #define Y second
 
@@ -8,6 +10,36 @@ using namespace std;
 struct Ride {
   pair<int, int> st;
   pair<int, int> fin;
+  pair<int, int> ranges;
+};
 
-  int ts, tf;
+struct Input {
+  int R, C, F, N, B, T;
+  vector<Ride> rides;
+};
+
+inline Input read_input(const std::string& fname) {
+  ifsteam in_f(fname);
+  Input in;
+  in_f >> in.R >> in.C >> in.F >> in.N >> in.B >> in.T;
+  in.rides.resize(in.N);
+  for (auto& ride : in.rides) {
+    in_f >> ride.st.X >> ride.st.Y >> ride.fin.X >> ride.fin.Y >> ride.ranges.X >> ride.ranges.Y;
+  }
+  return in;
+}
+
+class Solution {
+public:
+  Solution(const std::string& in_f):
+    Solution(read_input(in_f)) {}
+
+  Solution(Input&& input):
+    input_(move(input)) {}
+
+
+private:
+  void finish() { }
+
+  Input input_;
 };
