4962ab9c279a3d5484e95f7362ced19e29dbb16a
Dmitrii Zakharov <dimaz1301@gmail.com>
2019-01-13 17:04:01 +0300

    Fix solution

--- 2019/practice/solutions/small/solution.h
+++ 2019/practice/solutions/small/solution.h
@@ -9,9 +9,10 @@ public:
 
   void solve_internal() override {
     output().sls = {
-      {PT{0, 0}, PT{0, 6}},
+      {PT{0, 1}, PT{0, 6}},
+      {PT{0, 0}, PT{1, 0}},
       {PT{1, 1}, PT{1, 6}},
-      {PT{1, 0}, PT{5, 0}},
+      {PT{2, 0}, PT{5, 0}},
       {PT{2, 1}, PT{2, 6}},
       {PT{3, 1}, PT{3, 6}},
       {PT{4, 1}, PT{4, 6}},
