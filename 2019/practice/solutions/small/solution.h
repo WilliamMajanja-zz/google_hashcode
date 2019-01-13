7dab04d6234bc5e174ad13719bdc33bf0a18fef7
Dmitrii Zakharov <dimaz1301@gmail.com>
2019-01-13 16:56:31 +0300

    Solution for b_small.in

---
+++ 2019/practice/solutions/small/solution.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../../base/solution.h"
+
+class SmallSolution : public Solution {
+public:
+  SmallSolution():
+    Solution("../../input/b_small.in") {}
+
+  void solve_internal() override {
+    output().sls = {
+      {PT{0, 0}, PT{0, 6}},
+      {PT{1, 1}, PT{1, 6}},
+      {PT{1, 0}, PT{5, 0}},
+      {PT{2, 1}, PT{2, 6}},
+      {PT{3, 1}, PT{3, 6}},
+      {PT{4, 1}, PT{4, 6}},
+      {PT{5, 1}, PT{5, 6}}
+    };
+  }
+
+private:
+  const string class_name_ = "SmallSolution";
+};
+
