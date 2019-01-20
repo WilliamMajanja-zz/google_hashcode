5e00320e2f7eb2782a702d1b5f4a51caf8314b38
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-01-20 13:22:22 +0300

    base: fix async solution destructor:

--- 2017/qualification/base/async_solution.h
+++ 2017/qualification/base/async_solution.h
@@ -7,6 +7,8 @@ public:
   AsyncSolution(const Input& input, size_t number, atomic<size_t>& free_threads_count):
     input_(input), number_(number), free_threads_count_(free_threads_count) {}
 
+  virtual ~AsyncSolution() = default;
+
   const auto& input() const { return input_; }
   const auto& output() { return output_; }
   const auto& number() { return number_; }
