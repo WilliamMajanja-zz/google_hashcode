a5642f0c6b792a32eef63359037852fe94e6ba8f
Hippskill <hippskill@yandex-team.ru>
2019-01-20 13:32:19 +0300

    typo

--- 2017/qualification/base/statistics.h
+++ 2017/qualification/base/statistics.h
@@ -3,13 +3,14 @@
 #include "common.h"
 
 double calculate_score(const Input& input, const Output& output, bool enable_logging = true) {
+  /* validate and calculate score here */
+
   assert(input.C == output.servers.size() && "size of output.servers should be equal to input number of servers");
   for (const auto& serverVideos : output.servers) {
     assert(std::is_sorted(serverVideos.begin(), serverVideos.end()) && "videos in cache servers should be sorted");
   }
 
   double score = 0;
-  /* validate and calculate score here */
 
   for (const auto& request : input.requests) {
     const auto& endpoint = input.endpoints[request.E];
