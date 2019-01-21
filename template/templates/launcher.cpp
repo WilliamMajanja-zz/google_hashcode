e80577af2e3c16a3881e8274ab3502c160c02bf5
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-01-21 20:23:23 +0300

    templates: remove improving from launcher

--- template/templates/launcher.cpp
+++ template/templates/launcher.cpp
@@ -10,12 +10,4 @@ int main() {
   auto score = calculate_score(solution.input(), solution.output());
   LOG("<<test_name>> score: " << score)
   print_output(solution.output(), score, "../../output/<<test_name>>/");
-
-  auto improving_result = Improver::improve(solution.input(), solution.output());
-  while (improving_result.Y) {
-    improving_result = Improver::improve(solution.input(), move(improving_result.X));
-  }
-  auto improved_score = calculate_score(solution.input(), improving_result.X);
-  print_output(improving_result.X, improved_score, "../../output/<<test_name>>/");
-  LOG("<<test_name>> improved score: " << improved_score)
 }
