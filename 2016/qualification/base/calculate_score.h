e655b8a20e31692abd91d7f2e963c5602eb82ec8
Hippskill <hippskill@yandex-team.ru>
2019-01-27 14:26:50 +0300

    base: fix calculate score

--- 2016/qualification/base/calculate_score.h
+++ 2016/qualification/base/calculate_score.h
@@ -23,6 +23,8 @@ int get_distance(Postion lhs, Postion rhs) {
 }
 
 int calculate_score(const Input& input, const Output& output, bool enable_logging = true) {
+  Input input_copy = input;
+
   int score = 0;
 
   std::unordered_map<int, std::vector<Command>> drone_commands;
@@ -38,43 +40,47 @@ int calculate_score(const Input& input, const Output& output, bool enable_loggin
     int total_turns = 0;
     int load = 0;
 
-    Postion pos = {input.shops[0].row, input.shops[0].col};
+    Postion pos = {input_copy.shops[0].row, input_copy.shops[0].col};
     std::unordered_map<int, int> products_cnt;
 
     for (const auto& command : commands) {
       std::visit([&](auto&& cmd) {
           using T = std::decay_t<decltype(cmd)>;
           if constexpr (std::is_same_v<T, LoadCmd>) {
-            const auto& shop = input.shops[cmd.shop_id];
+            const auto& shop = input_copy.shops[cmd.shop_id];
             const Postion shop_pos = {shop.row, shop.col};
 
             total_turns += get_distance(pos, shop_pos) + 1;
             pos = shop_pos;
 
-            load += input.items[cmd.product_id] * cmd.number_of_items;
-            assert((load <= input.max_load) && "exceed max load for drone");
+            load += input_copy.items[cmd.product_id] * cmd.number_of_items;
+            assert((load <= input_copy.max_load) && "exceed max load for drone");
 
             products_cnt[cmd.product_id] += cmd.number_of_items;
+
+            input_copy.shops[cmd.shop_id].number_of_items[cmd.product_id] -= cmd.number_of_items;
+            assert((input_copy.shops[cmd.shop_id].number_of_items[cmd.product_id] >= 0) && "can't load more than shop have");
           } else if constexpr (std::is_same_v<T, UnloadCmd>) {
-            const auto& shop = input.shops[cmd.shop_id];
+            const auto& shop = input_copy.shops[cmd.shop_id];
             const Postion shop_pos = {shop.row, shop.col};
 
             total_turns += get_distance(pos, shop_pos) + 1;
             pos = shop_pos;
 
-            load -= input.items[cmd.product_id] * cmd.number_of_items;
-            assert((load <= input.max_load) && "exceed max load for drone");
+            load -= input_copy.items[cmd.product_id] * cmd.number_of_items;
 
             products_cnt[cmd.product_id] -= cmd.number_of_items;
             assert((products_cnt[cmd.product_id] >= 0) && "drone can't unload more than loaded");
+
+            input_copy.shops[cmd.shop_id].number_of_items[cmd.product_id] += cmd.number_of_items;
           } else if constexpr (std::is_same_v<T, DeliverCmd>) {
-            const auto& order = input.orders[cmd.order_id];
+            const auto& order = input_copy.orders[cmd.order_id];
             const Postion order_pos = {order.row, order.col};
 
             total_turns += get_distance(pos, order_pos) + 1;
             pos = order_pos;
 
-            load -= input.items[cmd.product_id] * cmd.number_of_items;
+            load -= input_copy.items[cmd.product_id] * cmd.number_of_items;
 
             products_cnt[cmd.product_id] -= cmd.number_of_items;
             assert((products_cnt[cmd.product_id] >= 0) && "drone can't deliver more than loaded");
@@ -87,20 +93,20 @@ int calculate_score(const Input& input, const Output& output, bool enable_loggin
           }
         }, command);
     }
-    assert((total_turns <= input.deadline) && "total number of turns for drone is more than input.deadline");
+    assert((total_turns <= input_copy.deadline) && "total number of turns for drone is more than input.deadline");
   }
 
-  for (size_t i = 0; i < input.orders.size(); ++i) {
+  for (size_t i = 0; i < input_copy.orders.size(); ++i) {
     std::unordered_map<int, int> ordered_cnt;
-    for (const auto& product : input.orders[i].product_items) {
+    for (const auto& product : input_copy.orders[i].product_items) {
       ++ordered_cnt[product];
     }
 
-    const auto& products = order_info[i].products;
-
-    if (!std::any_of(products.begin(), products.end(), [&ordered_cnt](const auto& it) {
-        return ordered_cnt[it.first] > it.second; })) {
-      score += static_cast<int>(std::ceil((input.deadline - order_info[i].max_turn) * 100. / input.deadline));
+    auto& products = order_info[i].products;
+    if (!std::any_of(ordered_cnt.begin(), ordered_cnt.end(), [&products](const auto& it) {
+        return products[it.first] < it.second; })) {
+      LOG("succefully finished order " << i << " in turn " << order_info[i].max_turn);
+      score += static_cast<int>(std::ceil((input_copy.deadline - order_info[i].max_turn) * 100. / input_copy.deadline));
     }
   }
 
