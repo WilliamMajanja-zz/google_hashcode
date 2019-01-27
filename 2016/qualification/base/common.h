7fbc3e736d249ee4b790eeaff2a251b6758dfbf0
Dmitrii Zakharov <dimaz1301@gmail.com>
2019-01-27 12:53:19 +0300

    Read input

--- 2016/qualification/base/common.h
+++ 2016/qualification/base/common.h
@@ -26,8 +26,28 @@ using namespace std;
 string class_name_;
 bool enable_logging = true;
 
+struct Warehouse {
+  int row;
+  int col;
+  vector<int> number_of_items;
+};
+
+struct Order {
+  int row;
+  int col;
+  vector<int> product_items;
+};
+
 struct Input {
-  /* input structure here */
+  int n_rows;
+  int n_cols;
+  int n_drones;
+  int deadline;
+  int max_load;
+
+  vector<int> order_weights;
+  vector<Warehouse> warehouses;
+  vector<Order> orders;
 };
 
 struct Output {
@@ -37,7 +57,47 @@ struct Output {
 inline Input read_input(const std::string& fname) {
   ifstream in_f(fname);
   Input in;
-  /* read input here */
+
+  in_f >> in.n_rows >> in.n_cols >> in.n_drones >> in.deadline >> in.max_load;
+  LOG("Field size: " << in.n_rows << "x" << in.n_cols);
+  LOG("Number of drones: " << in.n_drones);
+  LOG("Deadline: " << in.deadline);
+  LOG("Max load of each drone: " << in.max_load);
+
+  int P;
+  in_f >> P;
+  LOG("Number of different product types: " << P);
+  in.order_weights.resize(P);
+  for (int& weight : in.order_weights) {
+    in_f >> weight;
+  }
+
+  int W;
+  in_f >> W;
+  LOG("Number of warehouses: " << W);
+  in.warehouses.resize(W);
+  for (Warehouse& warehouse : in.warehouses) {
+    in_f >> warehouse.row >> warehouse.col;
+    warehouse.number_of_items.resize(P);
+    for (int& num_of_item : warehouse.number_of_items) {
+      in_f >> num_of_item;
+    }
+  }
+
+  int C;
+  in_f >> C;
+  LOG("Number of orders: " << C);
+  in.orders.resize(C);
+  for (Order& order : in.orders) {
+    in_f >> order.row >> order.col;
+    int L;
+    in_f >> L;
+    order.product_items.resize(L);
+    for (int& product_item_type : order.product_items) {
+      in_f >> product_item_type;
+    }
+  }
+
   return in;
 }
 
