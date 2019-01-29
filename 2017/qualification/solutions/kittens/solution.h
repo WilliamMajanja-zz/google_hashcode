73d3003e17ef4e6585e925c5083a88e756c6bb2f
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-01-29 14:37:21 +0300

    solutions: add top solution

--- 2017/qualification/solutions/kittens/solution.h
+++ 2017/qualification/solutions/kittens/solution.h
@@ -10,99 +10,76 @@ public:
 
   void solve_internal(const Input& input, Output& output) override {
     LOG("started")
-    output_.servers.resize(input.C);
-    vector<vector<pair<double, int>>> ser_to_vid(input.C);
-    vector<vector<pair<double, int>>> usr_to_vid(input.E);
-    for (auto req : input.requests) {
-      usr_to_vid[req.E].push_back(mk((double)req.N, req.V));
+    output.servers.resize(input.C);
+    vector<vector<double>> serv_to_vid(input.C, vector<double>(input.V));
+    vector<vector<Request>> req_by_vid(input.V);
+    vector<vector<bool>> used_req_by_vid(input.V);
+    vector<unordered_map<int, int>> end_to_serv(input.E);
+    for (const auto& req : input.requests) {
+      req_by_vid[req.V].push_back(req);
     }
     for (int i = 0; i < input.endpoints.size(); ++i) {
-      auto& usr = input.endpoints[i];
-      for (auto [serv, lat] : usr.connections) {
-        for (auto [num, vid] : usr_to_vid[i]) {
-          ser_to_vid[serv].push_back(
-              mk((double)num * (usr.L - lat), vid));
+      for (auto con : input.endpoints[i].connections) {
+        end_to_serv[i][con.first] = con.second;
+      }
+    }
+    for (int vid = 0; vid < input.V; ++vid) {
+      if (vid % 100 == 0) {
+        LOG("init vid: " << vid << " total: " << input.V)
+      }
+      for (int sid = 0; sid < input.C; ++sid) {
+        for (const auto& req : req_by_vid[vid]) {
+          if (end_to_serv[req.E].find(sid) != end_to_serv[req.E].end()) {
+            int old_lat = input.endpoints[req.E].L;
+            int new_lat = end_to_serv[req.E][sid];
+            serv_to_vid[sid][vid] += old_lat - new_lat;
+          }
         }
       }
     }
-    for (auto& serv : ser_to_vid) {
-      unordered_map<int, double> qwe;
-      for (auto [score, vid] : serv) {
-        qwe[vid] += score;
-      }
-      serv.resize(0);
-      for (auto [vid, score] : qwe) {
-        serv.push_back(mk(score, vid));
-      }
-    }
-    LOG("inited ser_to_vid")
-      /*
-    for (auto& serv : ser_to_vid) {
-      sort(serv.rbegin(), serv.rend());
-    }
-    LOG("sorted ser_to_vid")
-    */
-    vector<int> perm(input.C);
-    for (int i = 0; i < input.C; ++i) {
-      perm[i] = i;
-    }
-    random_shuffle(perm.begin(), perm.end());
+    LOG("serv_to_vid inited")
 
-    vector<bool> used(input.V);
-    vector<int> used_sz(input.C);
-
-    vector<double> dp(input.X + 1);
-    vector<int> ts(input.X + 1);
-    vector<vector<int>> parent(input.X + 1);
-    int timer = 1;
-    int cnt = 0;
-    for (auto sid : perm) {
-      LOG("process server: " << sid << " cnt: " << ++cnt << "/" << input.C)
-      ++timer;
-      dp[0] = 0;
-      ts[0] = timer;
-      parent[0] = {};
-      for (auto [score, vid] : ser_to_vid[sid]) {
-        int sz = input.videos[vid];
-        if (used[vid]) {
-          continue;
+    vector<int> sz(input.C);
+    while (1) {
+      int sid = 0;
+      int vid = 0;
+      for (int i = 0; i < input.C; ++i) {
+        for (int j = 0; j < input.V; ++j) {
+          if (serv_to_vid[i][j] > serv_to_vid[sid][vid]) {
+            sid = i;
+            vid = j;
+          }
         }
-        bool added = 0;
-        for (int i = input.X; i >= 0; --i) {
-          int to = input.videos[vid] + i;
-          if (ts[i] == timer && to <= input.X) {
-            if (ts[to] < timer || dp[to] < dp[i] + score) {
-              ts[to] = timer;
-              dp[to] = dp[i] + score;
-              parent[to] = parent[i];
-              parent[to].push_back(vid);
-              added = 1;
+      }
+      if (serv_to_vid[sid][vid] == 0) {
+        break;
+      }
+      sz[sid] += input.videos[vid];
+      if (input.videos[vid] + sz[sid] <= input.X) {
+        output.servers[sid].push_back(vid);
+        LOG("add video: " << vid << " to server: " << sid <<
+            " size: " << input.videos[vid] << 
+            " improve: " << serv_to_vid[sid][vid] << " free space: " << input.X - sz[sid])
+      }
+      serv_to_vid[sid][vid] = 0;
+
+      for (int i = 0; i < input.C; ++i) {
+        for (const auto& req : req_by_vid[vid]) {
+          if (end_to_serv[req.E].find(sid) != end_to_serv[req.E].end()) {
+            if (end_to_serv[req.E].find(i) != end_to_serv[req.E].end()) {
+              int old_lat = input.endpoints[req.E].L;
+              int new_lat = end_to_serv[req.E][i];
+              serv_to_vid[i][vid] -= old_lat - new_lat;
+              serv_to_vid[i][vid] = max(0.0, serv_to_vid[i][vid]);
             }
           }
         }
       }
-      int ind = 0;
-      double maxx = dp[ind];
-      for (int i = 0; i < input.X + 1; ++i) {
-        if (dp[i] > maxx && ts[i] == timer) {
-          ind = i;
-          maxx = dp[i];
-        }
-      }
-      output_.servers[sid].swap(parent[ind]);
-      for (auto vid : output_.servers[sid]) {
-        used[vid] = 1;
-      }
-      LOG(
-          "added videos count: " << output_.servers[sid].size() <<
-          " optimization score: " << maxx <<
-          " free space: " << input.X - ind)
     }
 
-    for (auto& serv : output_.servers) {
+    for (auto& serv : output.servers) {
       sort(serv.begin(), serv.end());
     }
-
     LOG("finished")
   }
 
