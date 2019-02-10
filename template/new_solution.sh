3f9af6dac4a946efb3cad149f3ac5c43d2939de4
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-10 13:20:26 +0300

    change new solution script

--- template/new_solution.sh
+++ template/new_solution.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -e
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./new_problem.sh <test_name>"
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
+    echo "Usage: ./new_problem.sh <test_name> [solution_name]"
     exit
 fi
 
-mkdir output/$1
-echo "output" > output/$1/output.txt
+solution_name=$1
 
-mkdir solutions/$1
-cp templates/* solutions/$1/
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/<<test_name>>/$1/g" solutions/$1/*
-else
-  sed -i -e "s/<<test_name>>/$1/g" solutions/$1/*
+if [ "$#" == 2 ]; then
+  solution_name=$2
 fi
-mkdir solutions/$1/logs
+
+mkdir output/$solution_name
+echo "output" > output/$solution_name/output.txt
+
+mkdir solutions/$solution_name
+cp templates/* solutions/$solution_name/
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e "s/<<test_name>>/$1/g" solutions/$solution_name/*
+else
+  sed -i -e "s/<<test_name>>/$1/g" solutions/$solution_name/*
+fi
+mkdir solutions/$solution_name/logs
