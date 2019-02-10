d589d952fa67bdc6873ec07a106833a443ebe231
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-10 16:06:59 +0300

    fail

---
+++ 2015/qualification/solutions/comb/megaassign.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../../base/common.h"
+
+vector<int> ser_to_pool(const Input& input, const vector<vector<int>>& rows) {
+  return {};
+  /*
+  vector<int> ans(input.M, -1);
+  int now;
+  int pl = 0;
+  {
+    now = 14;
+    for (int i = 0; i < rows[now]; ++i) {
+      ans[rows[now]] = pl;
+    }
+  }
+  {
+    now = 5;
+    for (int i = 0; i < rows[now]; ++i) {
+      ans[rows[now]] = pl;
+    }
+  }
+  return ans;
+  */
+}
