9d47b67e41f6b0c9a63b64b910d72b8a91e1d71d
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-10 18:54:50 +0300

    calculate_score: add validate method

--- template/base/calculate_score.h
+++ template/base/calculate_score.h
@@ -2,8 +2,16 @@
 
 #include "common.h"
 
+void validate(const Input& input, const Output& output, bool enable_logging = true) {
+  ASSERT(2 + 2 == 4, "something goes wrong")
+  /* validate output */
+  LOG("output format valid")
+}
+
 int calculate_score(const Input& input, const Output& output, bool enable_logging = true) {
+  validate(input, output, enable_logging);
+
   int score = 0;
-  /* validate and calculate score here */
+  /* calculate score here */
   return score;
 }
