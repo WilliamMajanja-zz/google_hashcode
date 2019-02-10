7d151e4b41a72fcb4241a13ab524289e86109f4d
Dmitrii Zakharov <dimaz1301@gmail.com>
2019-02-10 13:42:59 +0300

    Validate output size

--- 2015/qualification/base/calculate_score.h
+++ 2015/qualification/base/calculate_score.h
@@ -2,20 +2,28 @@
 
 #include "common.h"
 
+std::string print_coordinates(int r, int s) {
+  return "(" + std::to_string(r) + ", " + std::to_string(s) + ")";
+}
+
 void validate(const Input& input, const Output& output, bool enable_logging) {
+  assert(("Invalid output, expecting description of " + std::to_string(input.M) + " servers",
+    output.servs.size() == input.M));
   for (size_t i = 0; i < output.servs.size(); ++i) {
     const auto& server = output.servs[i];
     if (!server.ok)
       continue;
 
-    assert(("wrong coordinates of " + std::to_string(i) + " server: (" + std::to_string(server.ar)
-      + ", " + std::to_string(server.as) + ")", server.ar < 0 || input.R <= server.ar || server.as < 0 || input.S >= server.as));
+    assert(("wrong coordinates of " + std::to_string(i) + " server: " + print_coordinates(server.ar, server.as),
+      server.ar < 0 || input.R <= server.ar || server.as < 0 || input.S >= server.as));
 
-    assert(("server " + std::to_string(i) + " extends beyond the slots on the row ", server.as + input.servs[i].first > server.as));
+    assert(("server " + std::to_string(i) + " extends beyond the slots on the row ",
+      server.as + input.servs[i].first > server.as));
 
     for (size_t j = 0; j < input.us.size(); ++j) {
       if (input.us[j].first == server.ar) {
-        assert(("server " + std::to_string(i) + " occupies unavailable slot", server.as + input.servs[i].first <= input.us[j].second || server.as > input.us[j].second));
+        assert(("server " + std::to_string(i) + " occupies unavailable slot " + print_coordinates(input.us[j].first, input.us[j].second),
+          server.as + input.servs[i].first <= input.us[j].second || server.as > input.us[j].second));
       }
     }
   }
