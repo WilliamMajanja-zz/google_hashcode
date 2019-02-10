72a1fc7c0ba0a8bfcefcfecde01acfb11a0f0f93
Hippskill <hippskill@yandex-team.ru>
2019-02-10 15:33:20 +0300

    dc: add otzhing optimization

--- 2015/qualification/solutions/dc/solution.h
+++ 2015/qualification/solutions/dc/solution.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 #include "../../base/solution.h"
+#include "../../base/calculate_score.h"
 
 struct Space {
   int start_pos_;
@@ -72,6 +73,27 @@ public:
     return std::make_optional(space.start_pos_);
   }
 
+  void ReturnSpace(Space space) {
+    auto next = space_pool_.upper_bound(space);
+    if (next != space_pool_.end()) {
+      if (space.start_pos_ + space.length_ == next->start_pos_) {
+        space.length_ += next->length_;
+        space_pool_.erase(next);
+      }
+    }
+
+    auto before = space_pool_.lower_bound(Space(space.start_pos_ - 1, 0));
+    if (before != space_pool_.end()) {
+      if (before->start_pos_ + before->length_ == space.start_pos_) {
+        space.start_pos_ -= before->length_;
+        space.length_ += before->length_;
+        space_pool_.erase(before);
+      }
+    }
+
+    space_pool_.emplace(std::move(space));
+  }
+
   int Id() const {
     return id;
   }
@@ -88,11 +110,21 @@ private:
 class Solution : public BaseSolution {
 public:
   template <typename... T>
-  Solution(T&&... args):
-      BaseSolution(std::forward<T>(args)...) {}
+  Solution(T&&... args)
+    : BaseSolution(std::forward<T>(args)...)
+  {
+  }
 
   void solve_internal(const Input& input, Output& output) override {
     LOG("started")
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> rand_server(0, input.M - 1);
+    std::uniform_int_distribution<int> rand_row(0, input.R - 1);
+    std::uniform_int_distribution<int> rand_pool(0, input.P - 1);
+
     output.servs.resize(input.M);
 
     std::vector<std::vector<int>> dc_field(input.R, std::vector<int>(input.S, 0));
@@ -106,14 +138,6 @@ public:
       dc.emplace_back(dc_field[i], i);
     }
 
-    auto rng = std::default_random_engine {};
-    std::shuffle(dc.begin(), dc.end(), rng);
-/*
-    std::sort(dc.begin(), dc.end(), [](const auto& lhs, const auto& rhs) {
-      return lhs.FragmentationLevel() > rhs.FragmentationLevel();
-    });
-*/
-
     std::vector<ServerInfo> servers;
     servers.reserve(input.M);
     for (int i = 0; i < input.M; ++i) {
@@ -121,35 +145,89 @@ public:
       servers.emplace_back(size, cpu, i);
     }
     std::sort(servers.begin(), servers.end(), [](const auto& lhs, const auto& rhs) {
-      if (lhs.cpu_ != rhs.cpu_) {
-        return lhs.cpu_ > rhs.cpu_;
-      }
-      if (lhs.size_ != rhs.size_) {
-        return lhs.size_ < rhs.size_;
+      const double lhs_profit = static_cast<double>(lhs.cpu_) / std::sqrt(lhs.size_);
+      const double rhs_profit = static_cast<double>(rhs.cpu_) / std::sqrt(rhs.size_);
+      if (std::abs(lhs_profit - rhs_profit) > 1e-9) {
+        return lhs_profit > rhs_profit;
       }
       return lhs.id_ < rhs.id_;
     });
 
-    int cur_pool = 0;
-    int cur_row = 0;
-    for (const auto server : servers) {
-      bool ok = false;
+    auto try_to_insert_servers = [&] {
+      int cur_pool = rand_pool(gen);
+      int cur_row = rand_row(gen);
 
-      for (int j = 0; j < input.R; ++j) {
-        int row = cur_row;
+      for (const auto server : servers) {
+        if (output.servs[server.id_].ok) {
+          continue;
+        }
 
-        cur_row = (cur_row + 1) % input.R;
+        for (int j = 0; j < input.R; ++j) {
+          int row = cur_row;
 
-        auto res = dc[row].TryInsert(server);
-        if (res.has_value()) {
-          ok = true;
-          output.servs[server.id_] = Server(dc[row].Id(), res.value(), cur_pool);
-          break;
+          cur_row = (cur_row + 1) % input.R;
+
+          auto res = dc[row].TryInsert(server);
+          if (res.has_value()) {
+            output.servs[server.id_] = Server(dc[row].Id(), res.value(), cur_pool);
+            break;
+          }
+        }
+
+        cur_pool = (cur_pool + 1) % input.P;
+      }
+    };
+    try_to_insert_servers();
+
+    Output best_output = output;
+    int best_score = calculate_score(input, best_output, /* enable_logging */ false);
+
+    LOG("base score is " << best_score);
+
+    const double To = 100;
+    double T = To;
+    double dec = 10;
+    double h;
+    std::uniform_real_distribution<double> ud(0, 1.0);
+    for (int iter = 1; iter < 100000; ++iter) {
+      if (iter % 10000 == 0) {
+        LOG("T is " << T);
+      }
+      // take random servers and try to remove them from the output
+      constexpr int MAX_ATTEMPTS = 5; // just a magic const
+      for (int attempt = 0; attempt < MAX_ATTEMPTS; ++attempt) {
+        const int server_id = rand_server(gen);
+        if (!output.servs[server_id].ok) {
+          continue;
+        }
+
+        const int row = output.servs[server_id].ar;
+        const int start_pos = output.servs[server_id].as;
+        const int length = input.servs[server_id].first;
+        output.servs[server_id].ok = false;
+        dc[row].ReturnSpace(Space(start_pos, length));
+      }
+      try_to_insert_servers();
+
+      const int cur_score = calculate_score(input, output, /* enable_logging */ false);
+
+      if (cur_score > best_score) {
+        LOG("improved score " << cur_score);
+
+        best_score = cur_score;
+        best_output = output;
+      } else {
+        h = 1 / (1 + std::exp(-(cur_score - best_score) / T));
+        if (ud(gen) > h) {
+          output = best_output;
         }
       }
 
-      cur_pool = (cur_pool + 1) % input.P;
+      T = To * std::exp(-dec * std::pow(iter, 1.0 / (2 * input.R)));
     }
+
+    std::swap(output, best_output);
+    LOG("best score is " << best_score);
 
     LOG("finished")
   }
