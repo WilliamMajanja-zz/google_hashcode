4cfc9815838e71313003c7234c8ecb6e0dde2ca2
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-10 18:51:16 +0300

    improve logs

--- template/base/log.h
+++ template/base/log.h
@@ -2,3 +2,4 @@
 
 #define LOG(message) if (enable_logging) { clog << "[" << class_name_ << "::" << __func__ << "]: " << message << endl; }
 
+#define ASSERT(condition, message) { if (!(condition)) { LOG(message); exit(0); } }
