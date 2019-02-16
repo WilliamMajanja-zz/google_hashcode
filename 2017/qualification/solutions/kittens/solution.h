d5f9eea92731baeee6a391abd26eaad3baaf3a94
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-16 15:59:12 +0300

    templates: dublicate logs to file in run mode

--- 2017/qualification/solutions/kittens/solution.h
+++ 2017/qualification/solutions/kittens/solution.h
@@ -12,15 +12,9 @@ public:
   void solve_internal(const Input& input, Output& output) override {
     output.servers.resize(input.C);
 
-    DBG("inited vid_serv")
-    double vid_serv_score[input.V][input.C];
-    int vid_serv_pos[input.V][input.C];
-    for (int i = 0; i < input.V; ++i) {
-      for (int j = 0; j < input.C; ++j) {
-        vid_serv_pos[i][j] = vid_serv_score[i][j] = 0;
-      }
-    }
-    DBG("inited vid_serv")
+    vector<vector<double>> vid_serv_score(input.V, vector<double>(input.C));
+    vector<vector<int>> vid_serv_pos(input.V, vector<int>(input.C));
+    LOG("inited vid_serv")
 
     double connections[input.E][input.C];
     for (int i = 0; i < input.E; ++i) {
