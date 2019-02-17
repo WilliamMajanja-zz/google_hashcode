c7819364738738fec9078f900f7f9dc2f6d7c13c
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-17 16:57:13 +0300

    template: add all_run script

--- template/new_solution.sh
+++ template/new_solution.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
-if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
-    echo "Usage: ./new_problem.sh <test_name> [solution_name]"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./new_problem.sh solution_name default_test"
     exit
 fi
 
 solution_name=$1
+test_name=$2
 
-if [ "$#" == 2 ]; then
-  solution_name=$2
-fi
-
-mkdir output/$1
-echo "output" > output/$1/output.txt
 
 mkdir solutions/$solution_name
 cp templates/* solutions/$solution_name/
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/<<test_name>>/$1/g" solutions/$solution_name/*
+  sed -i '' -e "s/<<test_name>>/$test_name/g" solutions/$solution_name/*
 else
-  sed -i -e "s/<<test_name>>/$1/g" solutions/$solution_name/*
+  sed -i -e "s/<<test_name>>/$test_name/g" solutions/$solution_name/*
 fi
 mkdir solutions/$solution_name/logs
 echo "debug log" > solutions/$solution_name/logs/debug.log
+
+find input -name "*.in" -exec basename {} .in ';' | xargs -I {} bash -c "
+mkdir output/{};
+echo "output" > output/{}/output.txt;
+"
