415e6412981ee087ff2aee114c093f7664bd03d1
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-02-27 00:53:16 +0300

    improve fknap

--- 2018/qualification/utils/fast_knapsack.h
+++ 2018/qualification/utils/fast_knapsack.h
@@ -10,9 +10,13 @@ public:
   FastKnapsack(int capacity):
       kCapacity(capacity),
       cells_(capacity + 1),
+      time_(capacity + 1),
+      timer_(0),
       next_cells_(capacity + 1),
-      next_time_(capacity + 1) {
+      next_time_(capacity + 1),
+      next_timer_(0) {
     DBG("init capacity: " << kCapacity)
+    cells_way_.reserve(capacity * 2);
     reset();
   }
 
@@ -21,22 +25,24 @@ public:
   void print();
 
   int capacity() { return kCapacity; }
-  const Pack& best_pack() { return cells_[best_cell_]; }
-  kCostType best_cost() { return best_pack().cost; }
+  const shared_ptr<Pack>& best_pack() { return cells_[best_cell_]; }
+  kCostType best_cost() { return best_pack()->cost; }
 
-  virtual int free_space() { return kCapacity - best_pack().next_free_cell; }
+  virtual int free_space() { return kCapacity - best_pack()->next_free_cell; }
 
 protected:
   virtual std::string representation();
 
 private:
-  virtual optional<Pack> try_emplace(int item_index, const Pack& pack) { return nullopt; }
+  virtual optional<Pack> try_emplace(int item_index, shared_ptr<Pack> pack) { return nullopt; }
   virtual void reset_internal() {}
 
-  vector<Pack> cells_;
+  vector<shared_ptr<Pack>> cells_;
   vector<int> cells_way_;
+  vector<int> time_;
+  int timer_;
 
-  vector<Pack> next_cells_;
+  vector<shared_ptr<Pack>> next_cells_;
   vector<int> next_time_;
   int next_timer_;
 
@@ -49,83 +55,54 @@ private:
 
 template<typename kCostType>
 bool FastKnapsack<kCostType>::add_item(int index) {
-  //LOG("cells_way_ size: " << cells_way_.size())
   auto previous_best_pack_cost = best_cost();
 
   next_timer_++;
-  vector<int> next_cells_way;
-  next_cells_way.reserve(cells_way_.size());
 
-  for (int cell : cells_way_) {
-    const auto& pack = cells_[cell];
-    DBG("process pack: next_free_cell: " << pack.next_free_cell << " cost: " << pack.cost)
-    auto next_pack = try_emplace(index, pack);
+  int cells_count = cells_way_.size();
+  for (int cell_ind = 0; cell_ind < cells_count; ++cell_ind) {
+    auto cell = cells_way_[cell_ind];
+    const auto& pack_ptr = cells_[cell];
+    const auto& pack = *pack_ptr;
+    //DBG("process pack: next_free_cell: " << pack.next_free_cell << " cost: " << pack.cost)
+    auto next_pack = try_emplace(index, pack_ptr);
     if (next_pack) {
       int next_free_cell = next_pack->next_free_cell;
-      DBG("can emplace: next_free_cell: " << next_free_cell <<
-          " cost: " << next_pack->cost << " cnt: " << next_pack->items.size())
+      //DBG("can emplace: next_free_cell: " << next_free_cell <<
+      //    " cost: " << next_pack->cost << " cnt: " << next_pack->size)
       bool change = false;
       if (next_time_[next_free_cell] == next_timer_) {
-        if (*next_pack > next_cells_[next_free_cell]) {
+        if (*next_pack > *next_cells_[next_free_cell]) {
           change = true;
         }
       } else {
         change = true;
-        next_cells_way.push_back(next_free_cell);
+        cells_way_.push_back(next_free_cell);
       }
       if (change) {
         next_time_[next_free_cell] = next_timer_;
-        next_cells_[next_free_cell] = move(*next_pack);
-        if (next_cells_[next_free_cell] > cells_[best_cell_]) {
-          best_cell_ = next_free_cell;
-        }
+        next_cells_[next_free_cell] = make_shared<Pack>(move(*next_pack));
       }
-    } else {
-      break;
     }
   }
 
-
-  sort(next_cells_way.begin(), next_cells_way.end());
-  DBG("next_cells_way size: " << next_cells_way.size())
-
-  vector<int> final_cells_way;
-
-  auto next_it = next_cells_way.begin();
-  auto curr_it = cells_way_.begin();
-
-  while (next_it != next_cells_way.end() || curr_it != cells_way_.end()) {
-    if (curr_it == cells_way_.end()) {
-      final_cells_way.push_back(*next_it);
-      cells_[*next_it] = next_cells_[*next_it];
-      ++next_it;
-      continue;
-    }
-    if (next_it == next_cells_way.end()) {
-      final_cells_way.push_back(*curr_it);
-      ++curr_it;
-      continue;
-    }
-    if (*next_it < *curr_it) {
-      final_cells_way.push_back(*next_it);
-      cells_[*next_it] = next_cells_[*next_it];
-      ++next_it;
-    } else if (*curr_it < *next_it) {
-      final_cells_way.push_back(*curr_it);
-      ++curr_it;
-    } else {
-      if (next_cells_[*next_it] > cells_[*curr_it]) {
-        cells_[*curr_it] = next_cells_[*next_it];
-      }
-      final_cells_way.push_back(*curr_it);
-      ++curr_it;
-      ++next_it;
-    }
-  }
-  cells_way_.swap(final_cells_way);
+  sort(cells_way_.begin(), cells_way_.end());
+  cells_way_.resize(unique(cells_way_.begin(), cells_way_.end()) - cells_way_.begin());
 
   DBG("finished: best cost: " << best_cost() << " free space: " << free_space() <<
       " cells_way_ size: " << cells_way_.size())
+
+  for (auto cell : cells_way_) {
+    if (next_time_[cell] == next_timer_) {
+      if (time_[cell] < timer_ || *next_cells_[cell] > *cells_[cell]) {
+        time_[cell] = timer_;
+        cells_[cell] = next_cells_[cell];
+        if (*cells_[cell] > *cells_[best_cell_]) {
+          best_cell_ = cell;
+        }
+      }
+    }
+  }
   return previous_best_pack_cost != best_cost();
 }
 
@@ -135,14 +112,26 @@ void FastKnapsack<kCostType>::reset() {
   cells_way_.clear();
   cells_way_.push_back(0);
   best_cell_ = 0;
+  cells_.front() = make_shared<Pack>();
+
+  ++timer_;
+  time_[0] = timer_;
 }
 
 template<typename kCostType>
 string FastKnapsack<kCostType>::representation() {
-  int now = 0;
+  auto pack = best_pack();
   string representation;
-  for (const auto& [index, position] : best_pack().items) {
-    //ASSERT(now <= position.l, "interval intersect previous: " << position.l << ' ' << position.r)
+  int now = 0;
+  vector<const Pack*> packs;
+  while (pack->previous_pack) {
+    packs.push_back(pack.get());
+    pack = *pack->previous_pack;
+  }
+  reverse(packs.begin(), packs.end());
+  for (const auto* pack : packs) {
+    int index = pack->item.X;
+    auto position = pack->item.Y;
     while (now < position.l) {
       representation.push_back('.');
       ++now;
@@ -165,7 +154,7 @@ void FastKnapsack<kCostType>::print() {
   LOG("==========================")
   LOG("best pack: " << repr)
   LOG("cost: " << best_cost())
-  LOG("items: " << best_pack().items.size())
+  LOG("items: " << best_pack()->size)
   LOG("unused space: " << free_space())
   LOG("==========================")
 }
@@ -185,10 +174,12 @@ template<typename kCostType>
 struct FastKnapsack<kCostType>::Pack {
   kCostType cost;
   int next_free_cell;
-  vector<pair<int, Position>> items;
+  optional<shared_ptr<Pack>> previous_pack;
+  pair<int, Position> item;
+  int size;
 
   Pack():
-    cost(0), next_free_cell(0) {}
+    cost(0), next_free_cell(0), previous_pack(nullopt), size(0) {}
 
   bool operator>(const Pack& rhs) const {
     return cost > rhs.cost;
